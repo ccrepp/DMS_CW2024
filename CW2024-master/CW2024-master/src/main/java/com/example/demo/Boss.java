3807c56de37d7edc69d855d9a6dbe4f12ac0fbae
ccrep <cchunren.ccr@gmail.com>
2024-11-20 10:02:50 +0800

    amended Boss - modified HEALTH value from 100 -> 10 - added Boss defeat logic   - checks health, if 0 or less, destroy() - added isDestroyed to update HEALTH

--- CW2024-master/CW2024-master/src/main/java/com/example/demo/Boss.java
+++ CW2024-master/CW2024-master/src/main/java/com/example/demo/Boss.java
@@ -12,7 +12,7 @@ public class Boss extends FighterPlane {
 	private static final double BOSS_SHIELD_PROBABILITY = .002;
 	private static final int IMAGE_HEIGHT = 300;
 	private static final int VERTICAL_VELOCITY = 8;
-	private static final int HEALTH = 100;
+	private static final int HEALTH = 10;
 	private static final int MOVE_FREQUENCY_PER_CYCLE = 5;
 	private static final int ZERO = 0;
 	private static final int MAX_FRAMES_WITH_SAME_MOVE = 10;
@@ -61,6 +61,13 @@ public class Boss extends FighterPlane {
 		if (!isShielded) {
 			super.takeDamage();
 		}
+		if (getHealth() <= 0){
+			destroy();
+		}
+	}
+
+	public boolean isDestroyed(){
+		return getHealth() <= 0;
 	}
 
 	private void initializeMovePattern() {
