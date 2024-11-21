dfa2aebebcc76f76c267a2f70f5c14e365005d08
ccrep <cchunren.ccr@gmail.com>
2024-11-21 16:43:59 +0800

    amended Boss - added ShieldImage - updated Shield logic   - showShield when activated   - hideShield when deactivated - updated Shield position to align with Boss
    amended LevelTwo
    - added getRoot().getChildren().add(boss.getShieldImage) to allow for access to Shield

--- CW2024-master/CW2024-master/src/main/java/com/example/demo/Boss.java
+++ CW2024-master/CW2024-master/src/main/java/com/example/demo/Boss.java
@@ -9,7 +9,7 @@ public class Boss extends FighterPlane {
 	private static final double INITIAL_Y_POSITION = 400;
 	private static final double PROJECTILE_Y_POSITION_OFFSET = 75.0;
 	private static final double BOSS_FIRE_RATE = .04;
-	private static final double BOSS_SHIELD_PROBABILITY = .002;
+	private static final double BOSS_SHIELD_PROBABILITY = 0.02;
 	private static final int IMAGE_HEIGHT = 300;
 	private static final int VERTICAL_VELOCITY = 8;
 	private static final int HEALTH = 10;
@@ -25,6 +25,8 @@ public class Boss extends FighterPlane {
 	private int indexOfCurrentMove;
 	private int framesWithShieldActivated;
 
+	private final ShieldImage shieldImage;
+
 	public Boss() {
 		super(IMAGE_NAME, IMAGE_HEIGHT, INITIAL_X_POSITION, INITIAL_Y_POSITION, HEALTH);
 		movePattern = new ArrayList<>();
@@ -33,6 +35,8 @@ public class Boss extends FighterPlane {
 		framesWithShieldActivated = 0;
 		isShielded = false;
 		initializeMovePattern();
+
+		shieldImage = new ShieldImage(INITIAL_X_POSITION, INITIAL_Y_POSITION);
 	}
 
 	@Override
@@ -43,6 +47,8 @@ public class Boss extends FighterPlane {
 		if (currentPosition < Y_POSITION_UPPER_BOUND || currentPosition > Y_POSITION_LOWER_BOUND) {
 			setTranslateY(initialTranslateY);
 		}
+
+		getShieldImage().setLayoutY(currentPosition);
 	}
 	
 	@Override
@@ -80,9 +86,17 @@ public class Boss extends FighterPlane {
 	}
 
 	private void updateShield() {
-		if (isShielded) framesWithShieldActivated++;
-		else if (shieldShouldBeActivated()) activateShield();	
-		if (shieldExhausted()) deactivateShield();
+		if (isShielded) {
+			framesWithShieldActivated++;
+		}
+		else if (shieldShouldBeActivated()) {
+			activateShield();
+			shieldImage.showShield();
+		}
+		if (shieldExhausted()) {
+			deactivateShield();
+			shieldImage.hideShield();
+		}
 	}
 
 	private int getNextMove() {
@@ -122,6 +136,10 @@ public class Boss extends FighterPlane {
 	private void deactivateShield() {
 		isShielded = false;
 		framesWithShieldActivated = 0;
+	}
+
+	public ShieldImage getShieldImage() {
+		return shieldImage;
 	}
 
 }
