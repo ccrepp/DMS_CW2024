da1c836be7094438ebe82502bf66fdec2cfcccbf
ccrep <cchunren.ccr@gmail.com>
2024-11-29 16:51:38 +0800

    Added MORE 'Star Wars' Themed Resources and Level - added resources (images)   - greenlaser   - greenlaser1   - ISD   - ISDShield
    - added ISD and ISDProjectile classes
    
      - ISD extends FighterPlane
      - uses ISD as image source
      - has same logic as Boss
    
      - ISD extends FighterPlane
      - uses redlaser1 as image source
        - changes TieFighterProjectile to greenlaser1
      - has same logic as BossProjectile
    
    - added LevelISD
      - Star Wars themed BOSS level
      - extends LevelParent
      - same game logic as LevelTwo
      - utilises XWing and ISD instead of UserPlane and Boss
        - XWing currently NOT displaying

---
+++ CW2024-master/CW2024-master/src/main/java/com/example/demo/ISDShield.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class ISDShield extends ImageView {
+
+    private static final String IMAGE_NAME = "/com/example/demo/images/ISDShield.png";
+    private static final int SHIELD_SIZE = 350;
+
+    public ISDShield(double xPosition, double yPosition) {
+
+        try{
+            System.out.println("ShieldImage: Loading ISDShield");
+            this.setImage(new Image(getClass().getResource("/com/example/demo/images/ISDShield.png").toExternalForm()));
+            System.out.println("ShieldImage: ISDShield LOADED");
+        } catch (Exception e){
+            System.out.println("ShieldImage: ISDShield Loading FAILED" + e.getMessage());
+            throw e;
+        }
+
+        this.setLayoutX(xPosition);
+        this.setLayoutY(yPosition);
+        //this.setImage(new Image(IMAGE_NAME));
+
+        this.setVisible(false);
+        this.setFitHeight(SHIELD_SIZE);
+        this.setFitWidth(SHIELD_SIZE);
+    }
+
+    public void showShield() {
+        this.setVisible(true);
+    }
+
+    public void hideShield() {
+        this.setVisible(false);
+    }
+
+}
